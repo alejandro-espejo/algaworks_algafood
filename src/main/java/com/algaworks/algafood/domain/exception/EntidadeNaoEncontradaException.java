c48ddca103d65fb0ca528152a28e9bc13d6fd4c5
alejandro-espejo <alejandro_yujra@hotmail.com>
2022-04-05 10:22:15 -0300

    8.5 - Simplificando o código com o uso de @ResponseStatus em Exceptions

--- src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,7 +1,10 @@
 package com.algaworks.algafood.domain.exception;
 
-//@ResponseStatus(value = HttpStatus.NOT_FOUND)//, reason = "Entidade não encontrada")
-public class EntidadeNaoEncontradaException extends RuntimeException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
+public class EntidadeNaoEncontradaException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 	
