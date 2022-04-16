d45cfd5e06fd610b36832c9ac9617ab8a738db4b
Alejandro Yujra Espejo <alejandro_yujra@hotmail.com>
2022-04-16 20:55:15 -0300

    10.10 - Criando o problema da ordem de execução dos testes

--- src/test/java/com/algaworks/algafood/AlgafoodApiIT.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiIT.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,15 +19,32 @@ public class AlgafoodApiIT {
 	@LocalServerPort
 	private int port;
 	
+	@BeforeEach
+	public void setUp() {
+		// Habilita os logs quando há falha no teste
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+	}
+	
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
 		given()
-		.basePath("/cozinhas")
-		.port(port)
 		.accept(ContentType.JSON)
 		.when()
 		.get()
 		.then()
 		.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {	
+		given()
+		.accept(ContentType.JSON)
+		.when()
+		.get()
+		.then()
+		.body("", hasSize(4))
+		.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
 }
