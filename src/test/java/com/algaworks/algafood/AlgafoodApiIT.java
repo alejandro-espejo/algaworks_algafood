e5988e41765de756e9fbcf4a53db602092ee9b1b
Alejandro Yujra Espejo <alejandro_yujra@hotmail.com>
2022-04-16 21:34:37 -0300

    10.12 - Voltando o estado inicial do banco de dados para cada execução de teste com Callback do Flyway

--- src/test/java/com/algaworks/algafood/AlgafoodApiIT.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiIT.java
@@ -4,8 +4,10 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -18,33 +20,34 @@ public class AlgafoodApiIT {
 
 	@LocalServerPort
 	private int port;
-	
+
+	@Autowired
+	private Flyway flyway;
+
 	@BeforeEach
 	public void setUp() {
 		// Habilita os logs quando há falha no teste
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+
+		flyway.migrate();
 	}
-	
+
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		given()
-		.accept(ContentType.JSON)
-		.when()
-		.get()
-		.then()
-		.statusCode(HttpStatus.OK.value());
+		given().accept(ContentType.JSON).when().get().then().statusCode(HttpStatus.OK.value());
 	}
-	
+
 	@Test
-	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {	
-		given()
-		.accept(ContentType.JSON)
-		.when()
-		.get()
-		.then()
-		.body("", hasSize(4))
-		.body("nome", hasItems("Indiana", "Tailandesa"));
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		given().accept(ContentType.JSON).when().get().then().body("", hasSize(4)).body("nome",
+				hasItems("Indiana", "Tailandesa"));
+	}
+
+	@Test
+	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+		given().body("{ \"nome\": \"Chinesa\" }").contentType(ContentType.JSON).accept(ContentType.JSON).when().post()
+				.then().statusCode(HttpStatus.CREATED.value());
 	}
 }
