073a93a3471c667652a6938a57c436cf31818649
Alejandro Yujra Espejo <alejandro_yujra@hotmail.com>
2022-04-30 19:47:02 -0300

    13.7 - Tratando BindException ao enviar parâmetros de URL inválidos

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -39,6 +40,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Autowired
 	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, 
+			HttpStatus status, WebRequest request) {
+		return handleValidationInternal(ex, headers, status, request, ex.getBindingResult());
+	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
@@ -137,7 +144,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				.title(problemType.getTitle()).detail(detail);
 	}
 
-	// DESAFIO
 	private String joinPath(List<Reference> references) {
 		return references.stream().map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
 	}
@@ -198,33 +204,36 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
+		return handleValidationInternal(ex, headers, status, request, ex.getBindingResult());
 	}
 
-	private ResponseEntity<Object> handleValidationInternal(Exception ex, BindingResult bindingResult,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+//	private ResponseEntity<Object> handleValidationInternal(Exception ex, BindingResult bindingResult,
+//			HttpHeaders headers, HttpStatus status, WebRequest request) {
+//		return handleValidationInternal(ex, bindingResult, headers, status, request);
+//	}
+
+	private ResponseEntity<Object> handleValidationInternal(Exception ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request, BindingResult bindingResult) {
 		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
 		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+		
 		List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream().map(objectError -> {
 			String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
-
 			String name = objectError.getObjectName();
 
 			if (objectError instanceof FieldError) {
 				name = ((FieldError) objectError).getField();
 			}
-
 			return Problem.Object.builder().name(name).userMessage(message).build();
 		}).collect(Collectors.toList());
 
 		Problem problem = createProblemBuilder(status, problemType, detail).userMessage(detail).objects(problemObjects)
 				.build();
-
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 
 	@ExceptionHandler({ ValidacaoException.class })
 	public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, WebRequest request) {
-		return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+		return handleValidationInternal(ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, request, ex.getBindingResult());
 	}
 }
